9569f96c201c4dddf71db88faf0bb6c6b9a9bc78
clairezed <clairezuliani@protonmail.com>
2022-10-09 23:56:21 +0200

    Add enabled to special pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_26_202346) do
+ActiveRecord::Schema.define(version: 2022_10_09_213259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
