42d9a5677a789b1e9ac15a645164bac42636ef3a
clairezed <clairezuliani@protonmail.com>
2023-03-23 22:10:33 +0100

    update migration

--- db/migrate/20221009213259_change_default_settings.rb
+++ db/migrate/20221009213259_change_default_settings.rb
@@ -1,9 +1,9 @@
 class ChangeDefaultSettings < ActiveRecord::Migration[6.1]
   def up
     if Setting.enabled_features.include?('key_numbers')
-      MainPage.find_by(key: "key_number").update(enabled: true)
+      MainPage.find_by(key: "key_number").update(enabled: true) if MainPage.find_by(key: "key_number").present?
     else
-      MainPage.find_by(key: "key_number").update(enabled: false)
+      MainPage.find_by(key: "key_number").update(enabled: false) if MainPage.find_by(key: "key_number").present?
     end
   end
 
